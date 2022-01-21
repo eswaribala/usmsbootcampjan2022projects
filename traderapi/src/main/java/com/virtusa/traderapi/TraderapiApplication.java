2f6cd9ace2b769e7d0e7ab971f14c74e3cbfa93e
eswaribala <parameswaribala@gmail.com>
2022-01-21 22:54:02 +0530

    updated

---
+++ traderapi/src/main/java/com/virtusa/traderapi/TraderapiApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.traderapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraderapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraderapiApplication.class, args);
+	}
+
+}
