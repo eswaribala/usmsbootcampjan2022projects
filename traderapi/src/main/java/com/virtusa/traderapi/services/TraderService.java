dcc0195cfc846a28b0e842603771aca25e2da3c9
eswaribala <parameswaribala@gmail.com>
2022-01-25 19:44:54 +0530

    updated

--- traderapi/src/main/java/com/virtusa/traderapi/services/TraderService.java
+++ traderapi/src/main/java/com/virtusa/traderapi/services/TraderService.java
@@ -1,5 +1,61 @@
 package com.virtusa.traderapi.services;
 
-public class TraderService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.virtusa.traderapi.models.Bank;
+import com.virtusa.traderapi.models.Trader;
+import com.virtusa.traderapi.repositories.BankRepo;
+import com.virtusa.traderapi.repositories.TraderRepo;
+
+@Service
+public class TraderService {
+	@Autowired
+	private TraderRepo traderRepo;
+	@Autowired
+	private BankService bankService;
+	
+	//insert 	
+	public Trader addTrader(Trader trader,long bankId) {
+	
+		 Bank bank=this.bankService.getBankById(bankId);
+		 if(bank!=null) 
+			 trader.setBank(bank);			
+		 
+		 else
+			 trader.setBank(null);
+		 this.traderRepo.save(trader);
+		 return trader;
+		 
+		}
+	
+	//list all the traders
+	
+	public List<Trader> getAllTraders(){
+		return this.traderRepo.findAll();
+	}
+	
+	//list trader by Id
+	
+	public Trader getTraderById(long traderId) {
+		return this.traderRepo.findById(traderId).orElse(null);
+	}
+	
+	//delete
+
+	public boolean deleteTraderById(long traderId) {
+		boolean status=false;
+		this.traderRepo.deleteById(traderId);
+		if(this.getTraderById(traderId)==null)
+			status=true;
+		return status;
+	}
+	
+	//update
+	
+	public Trader updateTrader(Trader trader) {
+		return this.traderRepo.save(trader);
+	}
 }
