83b818a399f5245231a75a3e942845ce27b7cf21
eswaribala <parameswaribala@gmail.com>
2022-01-31 23:08:46 +0530

    updated

--- configserver/src/main/java/com/virtusa/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/virtusa/configserver/ConfigserverApplication.java
@@ -2,9 +2,10 @@ package com.virtusa.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-
+@EnableConfigServer
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
