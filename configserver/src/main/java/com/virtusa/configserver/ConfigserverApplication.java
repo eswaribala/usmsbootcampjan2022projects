0cd6d39a16d8f19a014821e07eff4e678488f189
eswaribala <parameswaribala@gmail.com>
2022-01-31 23:07:45 +0530

    updated

--- configserver/src/main/java/com/virtusa/configserver/ConfigserverApplication.java
+++ configserver/src/main/java/com/virtusa/configserver/ConfigserverApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
 public class ConfigserverApplication {
 
 	public static void main(String[] args) {
