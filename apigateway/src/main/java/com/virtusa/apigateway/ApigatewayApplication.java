9f43df24f9841aff1c7588ebe3eaedc804130793
eswaribala <parameswaribala@gmail.com>
2022-02-03 21:06:01 +0530

    updated

--- apigateway/src/main/java/com/virtusa/apigateway/ApigatewayApplication.java
+++ apigateway/src/main/java/com/virtusa/apigateway/ApigatewayApplication.java
@@ -2,8 +2,12 @@ package com.virtusa.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
 public class ApigatewayApplication {
 
 	public static void main(String[] args) {
