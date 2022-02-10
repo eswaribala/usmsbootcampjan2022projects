9f361fcce281933b7a6632aa52246264c8953291
eswaribala <parameswaribala@gmail.com>
2022-02-10 23:11:37 +0530

    updated

---
+++ cloudoauth2apigateway/src/main/java/com/virtusa/cloudoauth2apigateway/Cloudoauth2apigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.cloudoauth2apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cloudoauth2apigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cloudoauth2apigatewayApplication.class, args);
+	}
+
+}
