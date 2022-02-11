7652b39508ab1ee13ce444776f6f432f8dce598d
eswaribala <parameswaribala@gmail.com>
2022-02-11 20:33:50 +0530

    updated

---
+++ cloudoauth2apigateway/src/main/java/com/virtusa/cloudoauth2apigateway/controllers/ApiController.java
@@ -0,0 +1,23 @@
+package com.virtusa.cloudoauth2apigateway.controllers;
+
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.WebSession;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+public class ApiController {
+	@GetMapping(value = "/token")
+	public Mono<String> getHome(@RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient) {
+		return Mono.just(authorizedClient.getAccessToken().getTokenValue());
+	}
+
+	@GetMapping("/")
+	public Mono<String> index(WebSession session) {
+		return Mono.just(session.getId());
+	}
+
+}
