0807690ececf65103629dc1c3b78eded9ef7b1dc
eswaribala <parameswaribala@gmail.com>
2022-02-11 23:07:42 +0530

    updated

--- circuitbreaker/src/main/java/com/virtusa/circuitbreaker/CircuitbreakerApplication.java
+++ circuitbreaker/src/main/java/com/virtusa/circuitbreaker/CircuitbreakerApplication.java
@@ -2,8 +2,12 @@ package com.virtusa.circuitbreaker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class CircuitbreakerApplication {
 
 	public static void main(String[] args) {
