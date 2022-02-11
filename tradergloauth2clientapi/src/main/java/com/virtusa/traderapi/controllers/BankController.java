d658d1e939a556c712673c631e2eb9a2ab226c6a
eswaribala <parameswaribala@gmail.com>
2022-02-11 20:21:19 +0530

    updated

--- tradergloauth2clientapi/src/main/java/com/virtusa/traderapi/controllers/BankController.java
+++ tradergloauth2clientapi/src/main/java/com/virtusa/traderapi/controllers/BankController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,8 +21,11 @@ import org.springframework.web.bind.annotation.RestController;
 import com.virtusa.traderapi.models.Bank;
 import com.virtusa.traderapi.services.BankService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
 @RequestMapping("/banks")
+@Slf4j
 public class BankController {
 	@Autowired
 	private BankService bankService;
@@ -33,10 +40,14 @@ public class BankController {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Bank Not Created");
 		
 	}
-	
+	@PreAuthorize("hasAuthority('SCOPE_TEST')")
 	//get
 	@GetMapping(value="/",params = "version=1.0")
 	public List<Bank> getAllBanks(){
+		SecurityContext context = SecurityContextHolder.getContext();
+		Authentication authentication = context.getAuthentication();
+		log.info("Scopes: " + authentication.getAuthorities());
+
 		return this.bankService.getAllBanks();
 	}
 	
