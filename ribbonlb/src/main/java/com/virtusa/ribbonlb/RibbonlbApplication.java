e7020f977fe31707e7b67e88d99823a31e3ff19b
eswaribala <parameswaribala@gmail.com>
2022-02-15 22:28:48 +0530

    updated

---
+++ ribbonlb/src/main/java/com/virtusa/ribbonlb/RibbonlbApplication.java
@@ -0,0 +1,13 @@
+package com.virtusa.ribbonlb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonlbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RibbonlbApplication.class, args);
+	}
+
+}
---
+++ ribbonlb/src/test/java/com/virtusa/ribbonlb/RibbonlbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.virtusa.ribbonlb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RibbonlbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
