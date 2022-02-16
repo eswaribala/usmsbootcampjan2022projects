0b056cd964c618c6c842935a4241a71a277cfbb5
eswaribala <parameswaribala@gmail.com>
2022-02-16 22:50:22 +0530

    updated

---
+++ orderapi/src/main/java/com/usbootcamp/orderapi/controllers/OrderController.java
@@ -0,0 +1,28 @@
+package com.usbootcamp.orderapi.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.usbootcamp.orderapi.models.Order;
+import com.usbootcamp.orderapi.services.OrderService;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    @Autowired
+	private OrderService orderService;
+	
+    @PostMapping(value="/",params = "version=1.0")
+    public ResponseEntity<?> publishOrder(@RequestBody Order order){
+    	
+    	if(orderService.publishOrder(order))
+    		return ResponseEntity.status(HttpStatus.ACCEPTED).body("Order Published....");
+    	else
+    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Order Not Published....");
+    }
+}
