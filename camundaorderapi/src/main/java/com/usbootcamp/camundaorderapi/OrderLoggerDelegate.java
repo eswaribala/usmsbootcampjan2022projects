e6817d776a5d5a4d31234e633eb9af2d6f9dc877
eswaribala <parameswaribala@gmail.com>
2022-02-24 21:03:09 +0530

    updated

--- camundaorderapi/src/main/java/com/usbootcamp/camundaorderapi/OrderLoggerDelegate.java
+++ camundaorderapi/src/main/java/com/usbootcamp/camundaorderapi/OrderLoggerDelegate.java
@@ -1,10 +1,13 @@
 package com.usbootcamp.camundaorderapi;
 
+import java.time.LocalDate;
 import java.util.logging.Logger;
 
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
+
+import com.usbootcamp.camundaorderapi.models.Order;
 
 /**
  * This is an easy adapter implementation
@@ -12,9 +15,9 @@ import org.springframework.stereotype.Component;
  * from within a BPMN 2.0 Service Task.
  */
 @Component("orderlogger")
-public class LoggerDelegate implements JavaDelegate {
+public class OrderLoggerDelegate implements JavaDelegate {
  
-  private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
+  private final Logger LOGGER = Logger.getLogger(OrderLoggerDelegate.class.getName());
   
   public void execute(DelegateExecution execution) throws Exception {
     
@@ -27,6 +30,12 @@ public class LoggerDelegate implements JavaDelegate {
             + ", executionId=" + execution.getId()
             + ", variables=" + execution.getVariables()
             + " \n\n");
+    
+    Order order = new Order();
+    order.setOrderId(Long.parseLong(execution.getVariable("orderId").toString()));    
+    order.setOrderAmount(Long.parseLong(execution.getVariable("orderAmount").toString()));
+    order.setProductId(Long.parseLong(execution.getVariable("productId").toString()));
+    order.setOrderDate(LocalDate.parse(execution.getVariable("dop").toString()));
     
   }
 
