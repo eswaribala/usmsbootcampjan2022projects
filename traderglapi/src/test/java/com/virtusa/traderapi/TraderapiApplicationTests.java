b15425ec1d633dd4b58586f5373195305d783826
eswaribala <parameswaribala@gmail.com>
2022-02-25 21:49:39 +0530

    updated

--- traderglapi/src/test/java/com/virtusa/traderapi/TraderapiApplicationTests.java
+++ traderglapi/src/test/java/com/virtusa/traderapi/TraderapiApplicationTests.java
@@ -1,13 +1,23 @@
 package com.virtusa.traderapi;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.virtusa.traderapi.models.Trader;
 
 @SpringBootTest
 class TraderapiApplicationTests {
 
+	private static Trader trader;
+	@BeforeAll
+	 static void createTraderInstance() {
+		trader=new Trader();
+	}
 	@Test
-	void contextLoads() {
+	void testTraderInstanceNotNull() {
+	 Assert.assertNotNull(trader);
 	}
 
 }
--- traderjunitapi/src/test/java/com/virtusa/traderapi/TraderapiApplicationTests.java
+++ traderjunitapi/src/test/java/com/virtusa/traderapi/TraderapiApplicationTests.java
@@ -3,8 +3,12 @@ package com.virtusa.traderapi;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.platform.commons.util.StringUtils;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.virtusa.traderapi.models.FullName;
 import com.virtusa.traderapi.models.Trader;
 
 @SpringBootTest
@@ -19,5 +23,12 @@ class TraderapiApplicationTests {
 	void testTraderInstanceNotNull() {
 	 Assert.assertNotNull(trader);
 	}
+	@ParameterizedTest
+    @ValueSource(strings = { "user1", "user84587", "test485874" })
+    void hasTexts(String firstName) {
+		trader.setName(new FullName(firstName,"",""));
+        Assert.assertTrue(trader.getName().getFirstName().startsWith("t"));
+    }
+	
 
 }
