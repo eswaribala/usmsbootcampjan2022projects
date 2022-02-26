7dfc402c60df7a11b2e5db7a883409d90da628fb
eswaribala <parameswaribala@gmail.com>
2022-02-26 22:39:51 +0530

    updated

--- traderjunitapi/src/test/java/com/virtusa/traderapi/TraderControllerTest.java
+++ traderjunitapi/src/test/java/com/virtusa/traderapi/TraderControllerTest.java
@@ -4,11 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.virtusa.traderapi.controllers.TraderController;
 
 @ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class TraderControllerTest {
 
 	@Autowired
