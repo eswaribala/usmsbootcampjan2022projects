9ec779de9340af2c3cab35d99f689366e7cf4973
eswaribala <parameswaribala@gmail.com>
2022-02-28 22:10:41 +0530

    updated

--- rest-client-demo/src/main/java/com/techragesh/restclientdemo/service/StudentService.java
+++
@@ -1,9 +0,0 @@
-package com.techragesh.restclientdemo.service;
-
-import com.techragesh.restclientdemo.model.Student;
-
-import java.util.List;
-
-public interface StudentService {
-    List<Student> getAllStudents();
-}
