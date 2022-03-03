3d10e513123b6df17fd814d5ab1f212b3cd65d4d
eswaribala <parameswaribala@gmail.com>
2022-03-03 22:33:58 +0530

    updated

---
+++ globalinsurance/src/main/java/com/vusbc/globalinsurance/CamundaApplication.java
@@ -0,0 +1,15 @@
+package com.vusbc.globalinsurance;
+
+import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableProcessApplication("globalinsurance")
+public class CamundaApplication {
+
+  public static void main(String... args) {
+    SpringApplication.run(CamundaApplication.class, args);
+  }
+
+}
---
+++ globalinsurance/src/main/java/com/vusbc/globalinsurance/ProcessConstants.java
@@ -0,0 +1,7 @@
+package com.vusbc.globalinsurance;
+
+public class ProcessConstants {
+
+  public static final String PROCESS_DEFINITION_KEY = "globalinsurance"; // BPMN Process ID
+
+}
