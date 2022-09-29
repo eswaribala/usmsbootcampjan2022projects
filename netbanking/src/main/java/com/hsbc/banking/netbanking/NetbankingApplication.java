67788f90567abded35bc9e8f4b09d6e1d48a1b1d
eswaribala <parameswaribala@gmail.com>
2022-09-29 15:02:23 +0530

    updated

---
+++ netbanking/src/main/java/com/hsbc/banking/netbanking/NetbankingApplication.java
@@ -0,0 +1,13 @@
+package com.hsbc.banking.netbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetbankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetbankingApplication.class, args);
+	}
+
+}
---
+++ netbanking/src/test/java/com/hsbc/banking/netbanking/NetbankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsbc.banking.netbanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetbankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
